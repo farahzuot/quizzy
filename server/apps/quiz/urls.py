6991f0a71205f5166c38f8e0c5b3c3782e75db1f
farahzuot <farahalzuot97@gmail.com>
2024-03-14 15:04:01 +0300

    refactor views of quiz app to be class based views

--- server/apps/quiz/urls.py
+++ server/apps/quiz/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path
 
 from server.apps.quiz.views import (
-    CreateQuiz,
     ListQuizView,
-    add_question,
-    create_quiz_settings,
+    AddQuestion,
+    CreateQuizSettings,
 )
 
 app_name = "quiz"
 
 urlpatterns = [
     path(
-        "", ListQuizView.as_view(template_name="quiz/quiz_list.html"), name="List_quiz"
+        "", ListQuizView.as_view(template_name="quiz/quiz_list.html"), name="list_quiz"
     ),
-    # path('create/', CreateQuiz.as_view(template_name='quiz/quiz_create.html'), name='create_quiz'),
-    path("create/", create_quiz_settings, name="create_quiz"),
-    path("add-question/", add_question, name="add_question"),
+    path("create/", CreateQuizSettings.as_view(), name="create_quiz"),
+    path("add-question/", AddQuestion.as_view(), name="add_question"),
 ]
--- server/apps/quiz/views.py
+++ server/apps/quiz/views.py
@@ -1,100 +1,94 @@
-import re
-
-from django.forms import inlineformset_factory
-from django.http import QueryDict
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, TemplateView, UpdateView
+from django.views import View
+from django.views.generic import ListView, FormView
 
 from _keenthemes.__init__ import KTLayout
 from _keenthemes.libs.theme import KTTheme
 from server.apps.quiz.forms import OptionFormSet, QuestionForm, QuizForm
 from server.apps.quiz.models import Option, Question, Quiz
 
+
 # Create your views here.
 
 
-class CreateQuiz(TemplateView):
-    template_name = "quiz/quiz_create.html"
+class CreateQuizSettings(FormView):
+    form_class = QuizForm
+    template_name = "quiz/quiz_create_settings.html"
+
+    def form_valid(self, form):
+        quiz = form.save(commit=False)
+        quiz.author = self.request.user.author
+        quiz.save()
+        self.request.session["quiz_id"] = str(quiz.id)
+        return redirect("quiz:add_question")
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-
-        """
-        # Example to get page name. Refer to dashboards/urls.py file.
-        url_name = resolve(self.request.path_info).url_name
-
-        if url_name == 'dashboard-2':
-            # Example to override settings at the runtime
-            settings.KT_THEME_DIRECTION = 'rtl'
-        else:
-            settings.KT_THEME_DIRECTION = 'ltr'
-        """
-
-        # A function to init the global layout. It is defined in _keenthemes/__init__.py file
         context = KTLayout.init(context)
-        KTTheme.addCssFile("css/create_quiz.css")
-        KTTheme.addJavascriptFile("plugins/custom/formrepeater/formrepeater.bundle.js")
-        KTTheme.addJavascriptFile("js/create_quiz.js")
         return context
 
 
-def create_quiz_settings(request):
-    if request.method == "POST":
-        form = QuizForm(request.POST)
-        if form.is_valid():
-            quiz = form.save(commit=False)
-            quiz.author = request.user.author
-            quiz.save()
-            request.session["quiz_id"] = str(quiz.id)
+class AddQuestion(View):
+    template_name = "quiz/quiz_add_question.html"
+    css_files = ["css/create_quiz.css"]
+    js_files = ["plugins/custom/formrepeater/formrepeater.bundle.js",
+                "js/create_quiz.js"]
+
+    def get(self, request):
+        quiz_id = self.get_quiz_id(request)
+        if not quiz_id:
+            return redirect("create_quiz_settings")
+        context = self.get_context_data(request)
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        quiz_id = self.get_quiz_id(request)
+        if not quiz_id:
+            return redirect("create_quiz_settings")
+        context = self.get_context_data(request)
+        if self.forms_are_valid(request, context):
+            self.save_question_and_options(context, quiz_id)
             return redirect("quiz:add_question")
-    else:
-        form = QuizForm()
+        return render(request, self.template_name, context)
 
-    context = {"form": form}
-    context = KTLayout.init(context)
+    def forms_are_valid(self, request, context):
+        return (
+            context['question_form'].is_valid()
+            and context['option_formset'].is_valid()
+        )
 
-    return render(request, "quiz/quiz_create_settings.html", context)
+    def save_question_and_options(self, context, quiz_id):
+        question = context['question_form'].save(commit=False)
+        question.quiz = get_object_or_404(Quiz, id=quiz_id)
+        question.save()
+        for form in context['option_formset']:
+            option_text = form.cleaned_data.get("option_text")
+            is_correct = form.cleaned_data.get("is_correct")
+            if option_text:
+                Option.objects.create(
+                    question=question,
+                    option_text=option_text,
+                    is_correct=is_correct,
+                )
 
+    def get_quiz_id(self, request):
+        return request.session.get("quiz_id")
 
-def add_question(request):
-    quiz_id = request.session.get("quiz_id")
-    if not quiz_id:
-        return redirect("create_quiz_settings")
-    if request.method == "POST":
-        question_form = QuestionForm(request.POST)
-        option_formset = OptionFormSet(request.POST)
-        if question_form.is_valid() and option_formset.is_valid():
-            question = question_form.save(commit=False)
-            question.quiz = get_object_or_404(Quiz, id=quiz_id)
-            question.save()
-            for form in option_formset:
-                option_text = form.cleaned_data.get("option_text")
-                is_correct = form.cleaned_data.get("is_correct")
-                if option_text:
-                    Option.objects.create(
-                        question=question,
-                        option_text=option_text,
-                        is_correct=is_correct,
-                    )
-            return redirect("quiz:add_question")
-
-    else:
-        question_form = QuestionForm()
-        option_formset = OptionFormSet()
-    questions_list = Question.objects.filter(quiz__id=quiz_id)
-    print("questions_list", questions_list)
-    context = {
-        "question_form": question_form,
-        "option_formset": option_formset,
-        "questions_list": questions_list,
-    }
-    context = KTLayout.init(context)
-    KTTheme.addCssFile("css/create_quiz.css")
-    KTTheme.addJavascriptFile("plugins/custom/formrepeater/formrepeater.bundle.js")
-    KTTheme.addJavascriptFile("js/create_quiz.js")
-    return render(request, "quiz/quiz_add_question.html", context)
+    def get_context_data(self, request):
+        question_form = QuestionForm(request.POST or None)
+        option_formset = OptionFormSet(request.POST or None)
+        questions_list = Question.objects.filter(quiz__id=self.get_quiz_id(request))
+        context = {
+            "question_form": question_form,
+            "option_formset": option_formset,
+            "questions_list": questions_list,
+        }
+        context = KTLayout.init(context)
+        for css_file in self.css_files:
+            KTTheme.addCssFile(css_file)
+        for js_file in self.js_files:
+            KTTheme.addJavascriptFile(js_file)
+        return context
 
 
 class ListQuizView(ListView):
